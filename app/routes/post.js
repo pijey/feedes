19b85fa5d4f0b459d7138ce2d243481fb786c150
pijey <pjvilloud-bitbucket@yahoo.fr>
2015-04-27 09:15:12 +0200

    Minor changes in home page

--- app/routes/post.js
+++ app/routes/post.js
@@ -7,16 +7,20 @@ export default Ember.Route.extend({
 	afterModel: function(model, transition) {
 		transition.then(function(){
 			Ember.$('meta[name="twitter:url"]').attr('content', window.location.href);
+			Ember.$('meta[name="og:url"]').attr('content', window.location.href);
 		});
         
         if(model.get('title')){
         	Ember.$('meta[name="twitter:title"]').attr('content', model.get('title'));
+        	Ember.$('meta[name="og:title"]').attr('content', model.get('title'));
         }
         if(model.get('description')){
         	Ember.$('meta[name="twitter:description"]').attr('content', model.get('description'));
+        	Ember.$('meta[name="og:description"]').attr('content', model.get('description'));
         }
         if(model.get('image')){
         	Ember.$('meta[name="twitter:image"]').attr('content', model.get('image'));
+        	Ember.$('meta[name="og:image"]').attr('content', model.get('image'));
         }
         
     }
